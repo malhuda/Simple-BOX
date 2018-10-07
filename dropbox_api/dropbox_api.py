965902b3ab0e828c179f7ef31d3f38f7d5795372
Xarrow <zhangjian12424@gmail.com>
2018-10-07 20:48:27 +0800

    Init

--- dropbox_api/dropbox_api.py
+++ dropbox_api/dropbox_api.py
@@ -512,7 +512,7 @@ def cli():
     :return:
     """
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0',port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
 
 cli()
